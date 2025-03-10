f9529597ebe5e23f386d59ffdbc0319be781caff
palpaMX <lois.guilloux77@gmail.com>
2025-03-10 18:04:37 +0100

    I add first query for report it's not finish query= "insert into select"

--- src/gestion/dao/ReportsQuery.java
+++ src/gestion/dao/ReportsQuery.java
@@ -1,4 +1,26 @@
 package gestion.dao;
+/*
+import gestion.ui.SceneManager;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import java.sql.*;
+
 
 public class ReportsQuery {
+    public static void SupplierRepport()
+        try {
+            connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/projetjava", "root", "root" );
+            String query = "Insert INTO Reports (name_fournisseur,num_fournisseur) Select name_fournisseur, num_fournisseur from Supplier";
+            PreparedStatement pstmt = con.prepareStatement(query);
+            pstmt.setInt
+    }
 }
+*/
